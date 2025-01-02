4b0151aa719d5c663b034d2a2a77e9283adeaf9e
Nicolas-Saade <nicolasnsaade@gmail.com>
2025-01-01 20:51:23 -0500

    Saving FIrebase Setup prog

--- FrontEnd/ios/FrontEnd/AppDelegate.mm
+++ FrontEnd/ios/FrontEnd/AppDelegate.mm
@@ -1,11 +1,16 @@
 #import "AppDelegate.h"
-
 #import <React/RCTBundleURLProvider.h>
+#import <Firebase.h> // Import Firebase
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  // Initialize Firebase
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
+
   self.moduleName = @"FrontEnd";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
