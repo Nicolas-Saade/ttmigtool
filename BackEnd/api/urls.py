bd46389aac68e4edb49c26ddeac18a388834e54f
marcgermanos <marc.germanos@mail.mcgill.ca>
2025-01-10 18:33:00 -0500

    we can post an account what to do left: check that the account does not already exist in the db and login

--- BackEnd/api/urls.py
+++ BackEnd/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import upload_json_file, get_profile_mappings
+from .views import upload_json_file, get_profile_mappings, create_user_profile
 
 urlpatterns = [
         path('upload-json/', upload_json_file, name='upload_json_file'),
         path('profile-mapping/', get_profile_mappings, name='get_profile_mappings'),
+        path('user-profile/', create_user_profile, name='create_user_profile'),
 ]
