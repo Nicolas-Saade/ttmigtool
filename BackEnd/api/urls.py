049c828299b36ef19d21dd92cc50aa26994c5eab
marcgermanos <marc.germanos@mail.mcgill.ca>
2025-01-11 14:40:32 -0500

    user can now login but we do not see their files next commit

--- BackEnd/api/urls.py
+++ BackEnd/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import upload_json_file, get_profile_mappings, create_user_profile
+from .views import upload_json_file, get_profile_mappings, create_user_profile, check_email  # Added endpoint
 
 urlpatterns = [
         path('upload-json/', upload_json_file, name='upload_json_file'),
         path('profile-mapping/', get_profile_mappings, name='get_profile_mappings'),
         path('create-user-profile/', create_user_profile, name='create_user_profile'),
+        path('check-email/', check_email, name='check_email'),  # Added endpoint
 ]
