e308560c7004a96723a550becf06ce0b7aa72b61
MarcAbouAbdallah <marcabouabdallah112304@gmail.com>
2025-01-10 19:18:59 -0500

    Database scripts

--- BackEnd/Database/queries.py
+++ BackEnd/Database/queries.py
@@ -1,7 +1,14 @@
 import requests
 import csv
 from dotenv import load_dotenv
+#import os
+import sys
 import os
+
+# Add the parent directory to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from SupaBaseClient import supabase
 
 load_dotenv('../.env')
 
@@ -41,13 +48,13 @@ def google_search(query):
 csv_file_path = './top_tiktokers.csv'
 subscribers_list = get_subscribers_list_from_csv(csv_file_path)
 
-current_list = subscribers_list[:1] # PROGRESSIVELY CHANGE
+current_list = subscribers_list[1:10] # PROGRESSIVELY CHANGE
 print(current_list)
 
 for tiktok_username in current_list:
     insta_query = f"{tiktok_username} Instagram account"
     insta_link = google_search(insta_query)
-    print(f"Instgram of {tiktok_username}\nLink: {insta_link}")
+    print(f"Instagram of {tiktok_username}\nLink: {insta_link}")
 
     twitter_query = f"{tiktok_username} Twitter account"
     twitter_link = google_search(twitter_query)
@@ -56,5 +63,30 @@ for tiktok_username in current_list:
     facebook_query = f"{tiktok_username} Facebook account"
     facebook_link = google_search(facebook_query)
     print(f"Facebook of {tiktok_username}\nLink: {facebook_link}")
+
+    try:
+        # Check if an instance with the given username already exists
+        existing_record = supabase.table("socials_mapping").select("*").eq("tiktok_username", tiktok_username).execute()
+        
+        if existing_record.data:
+            print(f"Instance with tiktok username '{tiktok_username}' already exists.")
+        else:
+            # Insert a new record
+            try:
+                response = supabase.table("socials_mapping").insert({
+                "tiktok_username": tiktok_username,
+                "instagram_username": insta_link,
+                "x_username": twitter_link,
+                "facebook_username": facebook_link
+            }).execute()
+            
+            except Exception as e:
+                print(response({"error": e}, status=500))
+
+            if not response or not response.data:
+                print(response({"error": f"Invalid Insertion in database for {tiktok_username} ."}, status=404))
+    
+    except Exception as e:
+        print(f"An error occurred while checking or inserting the record: {e}")
 
 
