afc309bb036e7a49c346a9afba77f14f2c524368
marcgermanos <marc.germanos@mail.mcgill.ca>
2025-01-10 23:37:22 -0500

    we can now register successfully

--- BackEnd/api/urls.py
+++ BackEnd/api/urls.py
@@ -4,5 +4,5 @@ from .views import upload_json_file, get_profile_mappings, create_user_profile
 urlpatterns = [
         path('upload-json/', upload_json_file, name='upload_json_file'),
         path('profile-mapping/', get_profile_mappings, name='get_profile_mappings'),
-        path('user-profile/', create_user_profile, name='create_user_profile'),
+        path('create-user-profile/', create_user_profile, name='create_user_profile'),
 ]
