c4aff6b8d0f152b07875350b96eb28939766536c
marcgermanos <marc.germanos@mail.mcgill.ca>
2025-01-14 23:59:28 -0500

    encrypting all user passwords

--- BackEnd/api/urls.py
+++ BackEnd/api/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
         path('upload-json/', upload_json_file, name='upload_json_file'),
         path('profile-mapping/', get_profile_mappings, name='get_profile_mappings'),
         path('create-user-profile/', create_user_profile, name='create_user_profile'),
-        path('check-email/', check_email, name='check_email'),  # Added endpoint
+        path('check-email/', check_email, name='check_email'),  
 ]
